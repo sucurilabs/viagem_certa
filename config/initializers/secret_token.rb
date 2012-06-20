2389371669f65abd56e22ba8871da85a573bd5ca
yurimello <yuri.mcr@gmail.com>
2012-06-19 21:23:15 -0300

    Inicializando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViagemCerta::Application.config.secret_token = '95a84a2e9c1c62421651edaf0d98c149847753b4896af19fa54525b9dc0db442ed98af82c1cdab23c4847622a8dc863b8ca2a1080772582a7c561e00bf6995ea'
